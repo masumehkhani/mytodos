084c0bee7b6fc492423aa185c60049cd9e1ea403
masumeh <khanimasume88@gmail.com>
2023-05-10 10:28:05 +0430

    add handlers to button in modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 
-const Modal = () => {
+const Modal = ({ onCancel, onConfirm }) => {
+  const deleteHandler = () => {
+    onCancel();
+  };
+  const confirmlHandler = () => {
+    onConfirm();
+  };
   return (
-    <div className="absolute top-1/2 right-1/2 p-4 rounded  bg-[#f5f2f4]">
+    <div className="absolute top-1/2 right-1/2 p-4 rounded z-10 bg-[#f5f2f4]">
       <p className="w-full p-6 text-center text-1xl">Are you sure?</p>
       <button
+        onClick={deleteHandler}
         type="button"
         class="inline-block rounded bg-[#c65032] mx-2 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
       >
         Confirm
       </button>
       <button
+        onClick={confirmlHandler}
         type="button"
         class="inline-block rounded border-[#c65032] mx-2 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
       >
